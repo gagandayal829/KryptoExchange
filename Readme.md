f878780c8158f210a82d81bf0886f54933246fc5
gagandayal829 <gagandayal829@gmail.com>
2022-01-19 01:38:59 -0500

    Loading component done

--- Readme.md
+++ Readme.md
@@ -4,18 +4,36 @@
 
 ## Packages Used
 
-### Vite used with React
+**Run under client folder** 
+
+### Vite used with React (**Run under client folder** )
 
 ```
 npm init vite@latest
 ```
 
-### Tailwind CSS
+### Tailwind CSS (**Run under client folder** )
 
 ```
 npm install -D tailwindcss postcss autoprefixer
 npx tailwindcss init
 ```
+
+### React-icons (**Run under client folder** )
+
+**Note:** The ethers package will help us to interact with the blockchain and with our smart contract
+
+```
+npm install react-icons ethers
+```
+
+
+
+
+
+
+
+
 
 
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -28,8 +28,8 @@ const Navbar = () => {
             <div className='md:flex-[0.5] flex-initial justify-center items-center'>
                 <img src={logo} alt="logo" className='w-32 cursor-pointer' />
             </div>
-            <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex initial'>
-                {/*item is to loop through each item in Market, Exchange , Tutorials and Wallets*/}
+            <ul className='text-white md:flex hidden b list-none flex-row justify-between items-center flex initial'>
+                {/*item is to loop through each item in Market, Exchange , Tutorials and Wallets, item + index is used to get a unique value*/}
                 {["Market","Exchange","Tutorials","Wallets"].map((item, index ) => (
                     <NavbarItem key={item + index} title={item} />
                 ))}
@@ -45,9 +45,15 @@ const Navbar = () => {
                 {toggleMenu
                     ? <AiOutlineClose fontSize={28} className='text-white md:hidden cursor-pointer' onClick={()=> setToggleMenu(false)}/>
                     : <HiMenuAlt4 fontSize={28} className='text-white md:hidden cursor-pointer' onClick={()=> setToggleMenu(true)} />}
-                {/* When menu is toggled on we need to show it, the code inside of parenthesis after && is only going to show when togglemenu is set to true */}
+                {/* When menu is toggled on/true we need to show it, the code inside of parenthesis after && is only going to show when togglemenu is set to true */}
                 {toggleMenu && (
-                    <ul>
+                    
+                    <ul
+                        className='z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2x1 md:hidden list-none
+                        flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in'
+                    >
+                        
+                    
                         <li className='text-xl w-full my-2'>
                             <AiOutlineClose onClick ={() => setToggleMenu(false)} />
                         </li>
